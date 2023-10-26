07c21e4c14bf11817d07b0dc548e4854e1681ec1
jackharris2215 <jackharris2215@outlook.com>
2023-10-26 11:43:58 -0600

    chapter 11

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,9 +4,17 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ("age",)
+        fields = (
+            "username",
+            "email",
+            "age",
+        )
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
+        fields = (
+            "username",
+            "email",
+            "age",
+        )
